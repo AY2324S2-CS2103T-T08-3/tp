3d3d3b1cc324420c3a5cd8ac27ff8d16d9152187
TIANYAO ZHANG <88520881+ZHANGTIANYAO1@users.noreply.github.com>
2024-03-09 18:30:07 +0800

    Correct format

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -1,16 +1,21 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.Model;
 import seedu.address.logic.commands.exceptions.CommandException;
-public class MarkCommand extends Command{
+import seedu.address.model.Model;
+
+/**
+ * Marks a contact as dead.
+ */
+public class MarkCommand extends Command {
     public static final String COMMAND_WORD = "mark";
     public static final String MESSAGE_SUCCESS = "Contact has been marked as dead!";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Marks the contacts identified by the index number as dead.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Marks the contacts identified by the index number as dead.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     @Override
-    public CommandResult execute(Model model) throws CommandException{
+    public CommandResult execute(Model model) throws CommandException {
         throw new CommandException("This method have not been implemented yet");
     }
 }
